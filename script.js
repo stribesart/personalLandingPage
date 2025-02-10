7fe20b5971a5fb08c79272682eb82972785b93f5
Set Reyes <set_abner@hotmail.com>
2025-02-10 00:30:03 -0600

    Primer commit - Landing Page con JS Vanilla

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("ctaButton");
+
+    button.addEventListener("click", function () {
+        alert("¡Gracias por tu interés! Pronto nos pondremos en contacto.");
+    });
+});
